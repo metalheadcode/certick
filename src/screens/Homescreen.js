c28985f2bdfd0d19803c03c6d09f5a9d57bac833
Metal Head Coder <41572398+metalheadcode@users.noreply.github.com>
2021-05-04 10:45:24 +0800

    add homescreen

---
+++ src/screens/Homescreen.js
@@ -0,0 +1,24 @@
+import React from "react"
+import CTALayoutsTypeTwelve from "../layouts/CTALayouts/CTALayoutsTypeTwelve/CTALayoutsTypeTwelve"
+import FeaturesLayoutsTypeFourteen from "../layouts/FeaturesLayouts/FeaturesLayoutsTypeFourteen/FeaturesLayoutsTypeFourteen"
+import FeaturesLayoutsTypeNinth from "../layouts/FeaturesLayouts/FeaturesLayoutsTypeNinth/FeaturesLayoutsTypeNinth"
+import FeaturesLayoutsTypeSecond from "../layouts/FeaturesLayouts/FeaturesLayoutsTypeSecond/FeatureLayoutsTypeSecond"
+import FeaturesLayoutsTypeSeventeenth from "../layouts/FeaturesLayouts/FeaturesLayoutsTypeSeventeenth/FeaturesLayoutsTypeSeventeenth"
+import FeaturesLayoutsTypeTenth from "../layouts/FeaturesLayouts/FeaturesLayoutsTypeTenth/FeaturesLayoutsTypeTenth"
+import FeaturesLayoutsTypeTwelve from "../layouts/FeaturesLayouts/FeaturesLayoutsTypeTwelve/FeaturesLayoutsTypeTwelve"
+
+const Homescreen = () => {
+  return (
+    <>
+      <FeaturesLayoutsTypeSecond dark={false} />
+      <FeaturesLayoutsTypeNinth dark={true} />
+      <FeaturesLayoutsTypeTwelve dark={false} />
+      <FeaturesLayoutsTypeSeventeenth dark={true} />
+      <FeaturesLayoutsTypeFourteen />
+      <FeaturesLayoutsTypeTenth dark={true} />
+      <CTALayoutsTypeTwelve dark={false} />
+    </>
+  )
+}
+
+export default Homescreen
